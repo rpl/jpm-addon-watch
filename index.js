b78dd934e0f0ca8187b22d64b5436851dc5d6552
Luca Greco <luca.greco@alcacoop.it>
2015-02-11 18:58:55 +0100

    go back to the original dir on exit or exception + minor tweaks

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 require("jpm-addon"); // install jpm gcli helpers
 
+const system = require('sdk/system');
 const self = require('sdk/self');
 
 const gcli = require("jpm-addon/dev/gcli");
 const { env } = require("sdk/system/environment");
 
-console.log("ENV", env);
-
-if (env.WATCH_ADDON_PATH && env.WATCH_ADDON_ID) {
-  var addon_path = env.WATCH_ADDON_PATH;
+if (env.JPM_WATCH_ADDON_PATH && env.JPM_WATCH_ADDON_ID && env.JPM_WATCH_FILE) {
+  var addon_path = env.JPM_WATCH_ADDON_PATH;
+  var watched_path = env.JPM_WATCH_FILE;
 
   gcli.run("addon mount "+ addon_path);
 
   var watchFilePath = require('./watch/index.js');
-  console.log("WATCH FILE PATH", watchFilePath);
-  watchFilePath(addon_path, env.WATCH_INTERVAL, _ => {
-    gcli.run("addon reload "+ env.WATCH_ADDON_ID);
+
+  console.log("WATCH PATH", watched_path);
+
+  watchFilePath(watched_path, env.JPM_WATCH_INTERVAL, _ => {
+    gcli.run("addon reload "+ env.JPM_WATCH_ADDON_ID);
   });
+} else {
+  console.error("JPM WATCH is not watching to anything or is not properly configured!");
+  system.exit();
 }
